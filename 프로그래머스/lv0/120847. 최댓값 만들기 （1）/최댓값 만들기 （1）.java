3490b3063f11b4ccbfffd75335e3144782e2d11f
Heeesuu <103115883+Heeesuu@users.noreply.github.com>
2023-03-09 18:28:00 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.32 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] numbers) {
+        
+        Arrays.sort(numbers);
+        return numbers[numbers.length-2]*numbers[numbers.length-1];
+    }
+}
