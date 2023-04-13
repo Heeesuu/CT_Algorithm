6291fde7bc23b81b87291182b033d7e9c8239ed3
Heeesuu <103115883+Heeesuu@users.noreply.github.com>
2023-04-13 10:53:04 +0900

    [level 0] Title: 숫자 찾기, Time: 0.02 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,18 +1,17 @@
 class Solution {
     public int solution(int num, int k) {
-        int answer = 0;
         String answerStr = Integer.toString(num);
         int[] answerList = new int[(answerStr.length())];
 
         for (int i = 0; i < answerList.length; i++){
             answerList[i] = answerStr.charAt(i) -'0';
-        }
-        for (int i = 0; i < answerList.length; i++) {
+            
             if (answerList[i] == k) {
                 return i + 1;
             }
         }
-
+        
         return -1;
     }
 }
+
