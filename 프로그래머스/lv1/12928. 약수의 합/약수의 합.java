445f9875b9929b6c985503fe0d6968004e284bbe
Heeesuu <103115883+Heeesuu@users.noreply.github.com>
2023-04-18 20:49:11 +0900

    [level 1] Title: 약수의 합, Time: 0.11 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        ArrayList<Integer> arr = new ArrayList<>();
+
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                arr.add(i);
+            }
+        }
+        for (int i = 0; i < arr.size(); i++) {
+            answer += arr.get(i);
+        }
+
+        return answer;
+    }
+}
