354b623a5b7ff6ce55e12669aa307836854cb4a4
Heeesuu <103115883+Heeesuu@users.noreply.github.com>
2023-04-26 23:08:28 +0900

    [level 1] Title: 모의고사, Time: 4.63 ms, Memory: 83.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -0,0 +1,42 @@
+import java.util.stream.IntStream;
+
+public class Solution {
+    private static final int[][] RULES = {
+            {1, 2, 3, 4, 5},
+            {2, 1, 2, 3, 2, 4, 2, 5},
+            {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+    };
+
+    private int getPicked(int person, int problem) {
+        //rule에는 RULES[1] 이면 1번째 사람의 패턴인 {1,2,3,4,5}가 들어간다.
+        int[] rule = RULES[person];
+        int index = problem % rule.length;
+        return rule[index];
+    }
+
+    public int[] solution(int[] answers) {
+        int[] corrects = new int[3];
+        int max = 0;
+        
+        for (int problem = 0; problem < answers.length; problem++) {
+            int answer = answers[problem];
+            
+            for (int person = 0; person < 3; person++) {
+                int picked = getPicked(person, problem);
+                if (answer == picked) {
+                    if (++corrects[person] > max) {
+                        max = corrects[person];
+                    }
+                }
+            }
+        }
+        
+        final int maxCorrects = max;
+        return IntStream.range(0,3)
+                .filter(i -> corrects[i] == maxCorrects)
+                .map(i -> i + 1)
+                .toArray();
+    }
+    
+    
+}
