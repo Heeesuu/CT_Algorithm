26dcf53a426aa05cf2dbf4ed795598c11dc6c8e2
Heeesuu <103115883+Heeesuu@users.noreply.github.com>
2023-05-04 16:21:42 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 468 ms, Memory: 20668 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
+public class Main {
+ 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		int N = Integer.parseInt(br.readLine());
+ 
+		// 역삼각형 코드
+		for (int i = 0; i < N; i++) {	// 1 ~ N
+ 
+			// 공백
+			for (int j = 0; j < i; j++) {
+				System.out.print(" ");
+			}
+ 
+			// 별
+			for (int k = 0; k < (2 * N - 1) - (2 * i); k++) {
+				System.out.print("*");
+			}
+ 
+			System.out.println();
+		}
+ 
+		// 삼각형 코드
+		for (int i = 0; i < N - 1; i++) {	// N+1 ~ 2N-1
+        
+			// 공백
+			for (int j = 1; j < (N - 1) - i; j++) {	
+				System.out.print(" ");
+			}
+ 
+			// 별
+			for (int k = 0; k < 3 + 2 * i; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+ 
+}
+ 
