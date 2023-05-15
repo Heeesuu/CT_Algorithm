3df5704604093a9f390199a2361b9b7572ecf82a
Heeesuu <103115883+Heeesuu@users.noreply.github.com>
2023-05-15 11:18:11 +0900

    [Silver II] Title: DNA 비밀번호, Time: 348 ms, Memory: 25512 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -1,6 +1,4 @@
 
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -21,12 +19,11 @@ public class Main {
         int S = Integer.parseInt(st.nextToken());
         int P = Integer.parseInt(st.nextToken());
 
-        int result = 0;
-
         char[] A = new char[S];
         checkArr = new int[4];
         myArr = new int[4];
         checkSecret = 0;
+        int result = 0;
 
         A = br.readLine().toCharArray();
 
@@ -34,15 +31,12 @@ public class Main {
 
         for (int i = 0; i < 4; i++) {
             checkArr[i] = Integer.parseInt(st.nextToken());
-            if (checkArr[i] == 0) {
-                checkSecret++;
-            }
+            if (checkArr[i] == 0) checkSecret++;
         }
 
-        for (int i = 0; i < P; i++){
+        for (int i = 0; i < P; i++) {
             Add(A[i]);
         }
-
         if (checkSecret == 4) {
             result++;
         }
@@ -51,8 +45,7 @@ public class Main {
             int j = i - P;
             Add(A[i]);
             Remove(A[j]);
-
-            if (checkSecret == 4){
+            if (checkSecret == 4) {
                 result++;
             }
         }
@@ -60,36 +53,31 @@ public class Main {
         System.out.println(result);
         br.close();
 
+
     }
 
-    private static void Add(char c){
+    private static void Add(char c) {
         switch (c) {
             case 'A' :
                 myArr[0]++;
-                if (myArr[0] == checkArr[0]) {
+                if (myArr[0] == checkArr[0])
                     checkSecret++;
-                }
                 break;
-
             case 'C' :
                 myArr[1]++;
-                if (myArr[1] == checkArr[1]) {
+                if (myArr[1] == checkArr[1])
                     checkSecret++;
-                }
                 break;
 
             case 'G' :
                 myArr[2]++;
-                if (myArr[2] == checkArr[2]) {
+                if (myArr[2] == checkArr[2])
                     checkSecret++;
-                }
                 break;
-
             case 'T' :
                 myArr[3]++;
-                if (myArr[3] == checkArr[3]) {
+                if (myArr[3] == checkArr[3])
                     checkSecret++;
-                }
                 break;
         }
     }
@@ -97,30 +85,26 @@ public class Main {
     private static void Remove(char c) {
         switch (c) {
             case 'A' :
-                if (myArr[0] == checkArr[0]){
+                if (myArr[0] == checkArr[0])
                     checkSecret--;
-                }
                 myArr[0]--;
                 break;
 
             case 'C' :
-                if (myArr[1] == checkArr[1]){
+                if (myArr[1] == checkArr[1])
                     checkSecret--;
-                }
                 myArr[1]--;
                 break;
 
             case 'G' :
-                if (myArr[2] == checkArr[2]){
+                if (myArr[2] == checkArr[2])
                     checkSecret--;
-                }
                 myArr[2]--;
                 break;
 
             case 'T' :
-                if (myArr[3] == checkArr[3]){
+                if (myArr[3] == checkArr[3])
                     checkSecret--;
-                }
                 myArr[3]--;
                 break;
         }
