ec8d421998ee017fc666f84c4bc10d3dcd54e7f6
Heeesuu <103115883+Heeesuu@users.noreply.github.com>
2023-05-16 13:26:31 +0900

    [Bronze V] Title: 아스키 코드, Time: 204 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        char c = sc.nextLine().charAt(0);
+
+        int num = (int) c;
+
+        System.out.println(num);
+
+    }
+}
