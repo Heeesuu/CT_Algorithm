7908da4faac41f8ee3d1f93b23a19c7fbf521977
Heeesuu <103115883+Heeesuu@users.noreply.github.com>
2023-05-16 11:23:06 +0900

    [level 2] Title: 올바른 괄호, Time: 17.26 ms, Memory: 54.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            switch (c) {
+                case '(' -> stack.push(c);
+                case ')' -> {
+                    if (stack.isEmpty()) {
+                        return false;
+                    }
+                    stack.pop();
+                }
+
+
+            }
+        }
+        return stack.isEmpty();
+        }
+    }
