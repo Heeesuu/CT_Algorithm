669221f1d055308a04902a9020c0dacd309c6953
Heeesuu <103115883+Heeesuu@users.noreply.github.com>
2023-06-04 23:08:36 +0900

    [unrated] Title: 꼬리 문자열, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.py
+++ 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.py
@@ -1,10 +1,6 @@
 def solution(str_list, ex):
-    tail_string = ''
-    filtered_list = []
-    
-    for string in str_list:
-        if ex not in string:
-            filtered_list.append(string)
-    
-    tail_string = ''.join(filtered_list)
-    return tail_string
+    answer = ''
+    for x in str_list:
+        if ex in x: continue
+        answer+=x
+    return answer
