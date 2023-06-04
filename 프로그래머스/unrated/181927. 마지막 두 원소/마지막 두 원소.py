bfae95c3974f87cd109e921b98e8caad5a9539bb
Heeesuu <103115883+Heeesuu@users.noreply.github.com>
2023-06-04 23:02:04 +0900

    [unrated] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.py
@@ -0,0 +1,11 @@
+def solution(num_list):
+    answer = []
+    
+    if(num_list[-1] > num_list[-2]):
+        num_list.append(num_list[-1] - num_list[-2])
+    else:
+        num_list.append(num_list[-1] * 2)
+    
+    answer = num_list
+        
+    return answer
