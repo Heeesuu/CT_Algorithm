4a585b9e25470bd0f74b55011b2c4a17a27a7afa
Heeesuu <103115883+Heeesuu@users.noreply.github.com>
2023-08-22 10:24:48 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.21 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -1,15 +1,18 @@
 def solution(survey, choices):
+    types = {'R': 0, 'T': 0, 'C': 0, 'F': 0, 'J': 0, 'M': 0, 'A': 0, 'N': 0}
+    for i in range(len(choices)):
+        if choices[i] < 4:
+            types[survey[i][0]] += (choices[i] * 3) % 4
+        if choices[i] > 4:
+            types[survey[i][1]] += choices[i] % 4
+    type_key = list(types.keys())
+    
     answer = ''
-    dic= {"R" : 0,"T" : 0,"C" : 0,"F" : 0,"J" : 0,"M" : 0,"A" : 0,"N" : 0 }
-    
-    for s,c in zip(survey,choices):
-        if c>4:     dic[s[1]] += c-4
-        elif c<4:   dic[s[0]] += 4-c
-    
-    li = list(dic.items())
-    
-    for i in range(0,8,2):
-        if li[i][1] < li[i+1][1]: answer += li[i+1][0]
-        else:   answer += li[i][0]
-        
+    for i in range(0, len(type_key), 2):
+        if types[type_key[i]] > types[type_key[i+1]]:
+            answer += type_key[i]
+        elif types[type_key[i]] < types[type_key[i+1]]:
+            answer += type_key[i+1]
+        else:
+            answer += min(type_key[i], type_key[i+1])
     return answer
