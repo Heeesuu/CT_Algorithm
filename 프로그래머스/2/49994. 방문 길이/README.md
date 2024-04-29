16444b51b1356254afd74e7b7b65e1c7f2b94fea
Heeesuu <103115883+Heeesuu@users.noreply.github.com>
2024-04-29 15:17:53 +0900

    [level 2] Title: 방문 길이, Time: 17.98 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/README.md
+++ 프로그래머스/2/49994. 방문 길이/README.md
@@ -1,10 +1,10 @@
 # [level 2] 방문 길이 - 49994 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49994?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49994) 
 
 ### 성능 요약
 
-메모리: 74 MB, 시간: 32.89 ms
+메모리: 84 MB, 시간: 17.98 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 29일 09:46:51
+2024년 04월 29일 15:17:51
 
 ### 문제 설명
 
--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -6,39 +6,38 @@ class Solution {
         int answer = 0;
 
         String[] dirsArr = dirs.split("");
-        Set<String> set = new HashSet<>();
+		Set<String> set = new HashSet<String>();
+		
+		int x = 0;
+		int y = 0;
+		
+		for (int i = 0; i < dirsArr.length; i++) {
+            int tempX = x;
+            int tempY = y;
+			switch (dirsArr[i]) {
+			case "U":
+				if(y+1 <= 5) y++;
+				break;
+			case "D":
+				if(y-1 >= -5) y--;
+				break;
+			case "L":
+				if(x-1 >= -5) x--;
+				break;
+			case "R":
+				if(x+1 <= 5) x++;
+				break;
+			}
 
-        int x = 0, y = 0;
-
-        for (String dir : dirsArr) {
-            int nx = x, ny = y;
-            switch (dir) {
-                case "U":
-                    if (y + 1 <= 5) ny++;
-                    break;
-                case "D":
-                    if (y - 1 >= -5) ny--;
-                    break;
-                case "L":
-                    if (x - 1 >= -5) nx--;
-                    break;
-                case "R":
-                    if (x + 1 <= 5) nx++;
-                    break;
-            }
-
-            if (nx != x || ny != y) {
-                String path = "(" + x + "," + y + ")-(" + nx + "," + ny + ")";
-                String reversePath = "(" + nx + "," + ny + ")-(" + x + "," + y + ")";
-                if (!set.contains(path) && !set.contains(reversePath)) {
-                    set.add(path);
-                }
-                x = nx;
-                y = ny;
+            String key = "(" + tempX + "," + tempY + ")(" + x + "," + y + ")";       
+            String reverseKey = "(" + x + "," + y + ")(" + tempX + "," + tempY + ")";
+            if (!key.equals(reverseKey)) { 
+                set.add(key);
+                set.add(reverseKey);
             }
         }
-
-        answer = set.size();
+		
+		answer = set.size()/2;
         return answer;
-    }
+	}
 }
