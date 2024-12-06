ab265658388adf3aba5605bd185dff2397d3f217
Heeesuu <103115883+Heeesuu@users.noreply.github.com>
2024-12-06 15:04:34 +0900

    [Bronze V] Title: 시험 성적, Time: 76 ms, Memory: 12336 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val a = readln().toInt()
+    if (a >= 90) {
+        println("A")
+    } else if (a >= 80) {
+        println("B")
+    } else if (a >= 70) {
+        println("C")
+    } else if (a >= 60) {
+        println("D")
+    } else {
+        println("F")
+    }
+
+}
